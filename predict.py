b18ba2c947d946ff8c243e8e111c5212b0e136f2
Nguyen Thai Huu Huy <81891365+huynth1801@users.noreply.github.com>
2022-04-29 20:47:14 +0700

    fixed app predict

--- predict.py
+++ predict.py
@@ -6,6 +6,9 @@ import torch.nn as nn
 from options.test_options import TestOptions
 from utils.utils import create_generator
 from PIL import Image
+from utils.utils import save_sample_png
+import matplotlib.pyplot as plt
+
 
 # Options
 opt = TestOptions().parse()
@@ -28,12 +31,13 @@ def predict(img, mask, opt=None):
     # img_raw = np.array(img)
     img = cv2.imread(img)
     mask = cv2.imread(mask, 0)
+    mask = np.expand_dims(mask, axis=-1)
+    # print("Mask: ", mask.shape)
     img = cv2.resize(img, (128,128), interpolation = cv2.INTER_AREA)
     mask = cv2.resize(mask, (128,128), interpolation = cv2.INTER_AREA)
-    # print("Mask: ", mask.shape)
     # print(mask.shape)
-    h_raw, w_raw, _ = img.shape
-    h_t, w_t = h_raw // 8*8, w_raw // 8*8
+    # h_raw, w_raw, _ = img.shape
+    # h_t, w_t = h_raw // 8*8, w_raw // 8*8
 
     # img = img.resize((w_t, h_t))
     # img = np.array(img)
@@ -45,22 +49,44 @@ def predict(img, mask, opt=None):
     # mask = mask.resize((128, 128))
     # print(mask.shape)
     # mask = np.array(mask, dtype=np.float32)
-    mask = torch.from_numpy(mask)
+    mask = torch.from_numpy(mask.astype(np.float32) / 255.0).contiguous()
     # mask = mask.permute(2, 0, 1)
-    img = torch.from_numpy(img)
+    img = torch.from_numpy(img.astype(np.float32) / 255.0).unsqueeze(0).contiguous()
 
-    img = (img / 255 - 0.5) / 0.5
-    img = img[None]
+    # img = (img / 255 - 0.5) / 0.5
+    # img = img.unsqueeze(0)
     mask = mask[None, None]
 
 
     with torch.no_grad():
-        generated, second_out = model(img , mask)
+        first_out, second_out = model(img , mask)
     
-    generated = torch.clamp(generated, -1., 1.)
-    generated = (generated + 1) / 2 * 255.0
-    generated = generated.cpu().nupy().astype(np.uint8)
-    generated = generated[0].permute((1, 2, 0))
-    result = generated * mask + img * (1 - mask)
-    result = result.astype(np.unit8)
+    # second_out = torch.clamp(second_out, -1., 1.)
+    # second_out = (second_out + 1) / 2 * 255.0
+    # generated = generated.cpu().numpy().astype(np.uint8)
+    print(second_out.shape)
+    print(mask.shape)
+    # generated = generated.transpose((1, 2, 0))
+    result = img * (1 - mask) + second_out * mask
+    print(img.shape)
+    # result = result.permute(1, 2, 0).cpu().numpy()
+    # result = cv2.cvtColor(result, cv2.COLOR_BGR2RGB) 
+    # cv2.imwrite('result.png', result)
+    # img_list = [result]
+    # name_list = ["second_out"]
+    # save_sample_png(sample_folder = r"C:\Users\acer\deepfillv2_pytorch\outputs", sample_name = '%d' % (0 + 1), 
+    #             img_list = img_list, name_list = name_list, pixel_max_cnt = 255)
+
+    result = result * 255
+    # Process img_copy and do not destroy the data of img
+    img_copy = result.clone().data.permute(0, 2, 3, 1)[0, :, :, :].cpu().numpy()
+    img_copy = np.clip(img_copy, 0, 255.0)
+    img_copy = img_copy.astype(np.uint8)
+    # Save to certain path
+    save_img_name = '1' + '_' + 'second_out' + '.png'
+    save_img_path = os.path.join(r"C:\Users\acer\deepfillv2_pytorch\outputs", save_img_name)
+    print(img_copy.shape)
+    img_copy = cv2.cvtColor(img_copy, cv2.COLOR_BGR2RGB)
+    plt.imsave("predict.png", img_copy)
+    # cv2.imwrite(save_img_path, img_copy)
     print("DONE INPAINTING")
